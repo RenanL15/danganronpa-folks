7d222a064ffa8733ece95ff52152b136c908f809
Renan <renanalmeida201612@gmail.com>
2024-07-17 14:50:00 -0300

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <div className="flex flex-col items-center justify-center gap-3 p-20 text-xl bg-gray-900 max-sm:px-2 max-sm:py-16">
+    <div className="flex flex-col items-center justify-center gap-3 p-20 text-xl bg-black max-sm:px-2 max-sm:py-16">
       <div className="flex items-center gap-3">
-        <p className="font-['Electrolize'] text-white">
-          Developed by:{" "}
+        <p className="font-['Open_Sans'] text-white">
+          Developed with
+          <i className="text-gray-200 fa-solid fa-heart mx-2 text-base"></i>by{" "}
           <a
             href="https://github.com/ScryptRG"
             target="blank"
@@ -11,16 +12,8 @@ export default function Footer() {
           >
             ScryptRG
           </a>{" "}
+          &copy; 2023
         </p>
-        <i className="text-gray-200 fa-solid fa-heart"></i>
-      </div>
-      <div className="flex gap-5 mt-2">
-        <a href="https://github.com/ScryptRG" target="_blank">
-          <i className="text-white duration-100 fa-brands fa-github hover:text-gray-400"></i>
-        </a>
-        <a href="https://linkedin.com/in/renan-almeida-lopes" target="_blank">
-          <i className="text-white duration-100 fa-brands fa-linkedin hover:text-blue-600"></i>
-        </a>
       </div>
     </div>
   );
